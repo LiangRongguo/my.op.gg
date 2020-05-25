e7395b29f543d90c55613364e12e0e91831aabc6
LiangRongguo <l_rongguo@outlook.com>
2020-05-25 09:31:42 -0400

    initialize home-page

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -5,7 +5,22 @@ import "./homepage.styles.scss";
 class HomePage extends Component {
   state = {};
   render() {
-    return <div>HomePage</div>;
+    const imageUrl =
+      // "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Irelia_6.jpg";
+      "https://images2.alphacoders.com/964/964702.png";
+    return (
+      <div className="Home">
+        <div
+          className="img"
+          style={{
+            width: "70%",
+            height: "600px",
+            backgroundImage: `url(${imageUrl})`,
+          }}
+        ></div>
+        <span className="title">Welcome to Summoner's Rift</span>
+      </div>
+    );
   }
 }
 
